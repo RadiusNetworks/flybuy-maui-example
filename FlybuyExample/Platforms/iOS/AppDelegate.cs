1700483fadad334c10c5684ab601bdc327ea1d06
ajhewitt <al@radiusnetworks.com>
2024-05-28 17:08:44 -0400

    Fill in missing iOS code Fill out the code in the FlybuyService. Built and runs, customer update and order redemption confirmed.

--- FlybuyExample/Platforms/iOS/AppDelegate.cs
+++ FlybuyExample/Platforms/iOS/AppDelegate.cs
@@ -1,9 +1,31 @@
-﻿using Foundation;
+﻿using CoreLocation;
+using FlyBuy;
+using Foundation;
+using UIKit;
 
 namespace FlybuyExample;
 
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
+    CLLocationManager locationManager;
+
+    public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+    {
+        locationManager = new CLLocationManager();
+        locationManager.RequestWhenInUseAuthorization();
+
+        // Configure SDK
+        var token = "102.CODE";
+        var builder = FlyBuyConfigOptions.BuilderWithToken(token);
+        builder.SetDeferredLocationTracking(true);
+        FlyBuyCore.ConfigureWithOptions(builder.Build);
+
+        // Configure Pickup Module
+        FlyBuyPickupManager.Shared.Configure();
+
+        return base.FinishedLaunching(app, options);
+    }
+
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
--- FlybuyExample/Platforms/iOS/FlybuyService.cs
+++ FlybuyExample/Platforms/iOS/FlybuyService.cs
@@ -1,67 +1,261 @@
 using System.Collections.ObjectModel;
+using FlyBuy;
 
 namespace FlybuyExample;
 
 public class FlybuyService : IFlybuyService
 {
+    static ObservableCollection<Site> Sites { get; set; }
+    static ObservableCollection<Order> Orders { get; set; }
+
     public FlybuyService()
     {
-        
+        Sites = new ObservableCollection<Site>();
+        Orders = new ObservableCollection<Order>();
     }
-    
+
+    static FlyBuyCustomerInfo CustomerInfo(Customer customer) => new FlyBuyCustomerInfo(
+        customer.Name,
+        customer.CarType,
+        customer.CarColor,
+        customer.CarLicense,
+        customer.Phone);
+
     public Customer CurrentCustomer()
     {
-        return new Customer();
-        
+        FlyBuyCustomer flybuyCustomer = FlyBuyCore.Customer.Current;
+        if (flybuyCustomer == null)
+        {
+            return null;
+        }
+        else
+        {
+            FlyBuyCustomerInfo info = flybuyCustomer.Info;
+            return new Customer(
+                info.Name,
+                info.Phone,
+                info.CarType,
+                info.CarColor,
+                info.LicensePlate,
+                flybuyCustomer.Token);
+        }
     }
 
     public void CreateCustomer(Customer customer)
     {
-        
+        FlyBuyCore.Customer.Create(
+            CustomerInfo(customer),
+            true, true,
+            (FlyBuyCustomer customer1, Foundation.NSError error) =>
+            {
+                if (error != null)
+                {
+                    Console.WriteLine("Create customer error: " + error.LocalizedDescription);
+                }
+                else
+                {
+                    Console.WriteLine("Create customer success");
+                }
+            });
     }
 
     public void UpdateCustomer(Customer customer)
     {
-        
+        FlyBuyCore.Customer.Update(
+            CustomerInfo(customer),
+            (FlyBuyCustomer customer1, Foundation.NSError error) =>
+            {
+                if (error != null)
+                {
+                    Console.WriteLine("Update customer error: " + error.LocalizedDescription);
+                }
+                else
+                {
+                    Console.WriteLine("Update customer success");
+                }
+            });
     }
 
     public void FetchOrder(string code)
     {
-        
+        FlyBuyCore.Orders.FetchWithRedemptionCode(
+            code,
+            (FlyBuyOrder order1, Foundation.NSError error) =>
+            {
+                if (error != null)
+                {
+                    Console.WriteLine("Claim order error: " + error.LocalizedDescription);
+                }
+                else
+                {
+                    Console.WriteLine("Claim order success");
+                    FetchOrders();
+                }
+            });
     }
 
     public void ClaimOrder(Order order, Customer customer)
     {
-        
+        FlyBuyCore.Orders.ClaimWithRedemptionCode(
+            order.Code,
+            CustomerInfo(customer),
+            order.PickupType,
+            (FlyBuyOrder order1, Foundation.NSError error) =>
+            {
+                if (error != null)
+                {
+                    Console.WriteLine("Claim order error: " + error.LocalizedDescription);
+                }
+                else
+                {
+                    Console.WriteLine("Claim order success");
+                    FetchOrders();
+                }
+            });
     }
 
     public void CreateOrder(Order order, Customer customer)
     {
-        
+        DateTime epoch = DateTime.UnixEpoch;
+        TimeSpan ts = order.PickupStart.Subtract(epoch);
+        Foundation.NSDate start = new Foundation.NSDate((long)ts.TotalMilliseconds);
+        var pickupWindow = new FlyBuyPickupWindow(start, start);
+
+        FlyBuyCore.Orders.CreateWithSiteID(
+            order.SiteId(),
+            order.Number,
+            CustomerInfo(customer),
+            pickupWindow,
+            "created",
+            order.PickupType,
+            (FlyBuyOrder order1, Foundation.NSError error) =>
+            {
+                if (error != null)
+                {
+                    Console.WriteLine("Create order error: " + error.LocalizedDescription);
+                }
+                else
+                {
+                    Console.WriteLine("Create order success");
+                    FetchOrders();
+                }
+            });
     }
 
     public void UpdateOrder(Order order, string customerState)
     {
-        
+        FlyBuyCore.Orders.UpdateCustomerStateWithOrderID(
+            order.Id,
+            customerState,
+            (FlyBuyOrder order1, Foundation.NSError error) =>
+            {
+                if (error != null)
+                {
+                    Console.WriteLine("Update order error: " + error.LocalizedDescription);
+                }
+                else
+                {
+                    Console.WriteLine("Update order success");
+                }
+            });
     }
 
     public void FetchOrders()
     {
-        
+        FlyBuyCore.Orders.FetchWithCallback(
+            (Foundation.NSArray<FlyBuyOrder> order, Foundation.NSError error) =>
+            {
+                if (error != null)
+                {
+                    Console.WriteLine("Fetch orders error: " + error.LocalizedDescription);
+                }
+                else
+                {
+                    Console.WriteLine("Fetch orders success");
+                }
+            });
     }
 
     public ObservableCollection<Order> GetOrders()
     {
-        return null;
+        Orders.Clear();
+        foreach (FlyBuyOrder order in FlyBuyCore.Orders.Open)
+        {
+            Site site = new Site(
+                (int)order.SiteID,
+                order.SitePartnerIdentifier,
+                order.SiteName,
+                order.SiteDescription);
+
+            FlyBuyPickupWindow pickupWindow = order.PickupWindow;
+            if (pickupWindow != null)
+            {
+                double x = pickupWindow.Start.SecondsSince1970;
+                var startTime = new DateTime((long)x * 1000L);
+                Orders.Add(
+                    new Order(
+                        (int)order.Id,
+                        site,
+                        order.PartnerIdentifier,
+                        order.PickupType,
+                        startTime)
+                );
+            }
+            else
+            {
+                Orders.Add(
+                    new Order(
+                        (int)order.Id,
+                        site,
+                        order.PartnerIdentifier,
+                        order.PickupType)
+                );
+            }
+        }
+
+        return Orders;
     }
 
     public ObservableCollection<Site> GetSites()
     {
-        return null;
+        if (Sites.Count == 0)
+        {
+            FlyBuyCore.Sites.FetchAllWithQuery("",
+                (Foundation.NSArray<FlyBuySite> sites1, Foundation.NSError error) =>
+                {
+                    if (error != null)
+                    {
+                        Console.WriteLine("Site fetch error: " + error.LocalizedDescription);
+                    }
+                    else
+                    {
+                        FlyBuySite[] sites2 = sites1.ToArray<FlyBuySite>();
+                        foreach (FlyBuySite site in sites2)
+                        {
+                            Sites.Add(
+                                new Site(
+                                    (int)site.Id,
+                                    site.PartnerIdentifier,
+                                    site.Name,
+                                    site.Description)
+                            );
+                        }
+                        Console.WriteLine("Site fetch success");
+                    }
+                });
+        }
+
+        return Sites;
     }
 
     public void OnMessageReceived(IDictionary<string, object> data)
     {
-        
+        Foundation.NSDictionary<Foundation.NSString, Foundation.NSString> q = new Foundation.NSDictionary<Foundation.NSString, Foundation.NSString>();
+        foreach (var p in data)
+        {
+            System.Diagnostics.Debug.WriteLine($"{p.Key} : {p.Value}");
+            q.TryAdd(new Foundation.NSString(p.Key), new Foundation.NSString(p.Value.ToString()));
+        }
+        FlyBuyCore.HandleRemoteNotification(q);
     }
 }
